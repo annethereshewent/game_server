e1fbfd20f1e4a449d9ee77026cb6bf6555e654a2
Anne Castrillon <anne.castrillon@gmail.com>
2017-11-08 19:59:13 -0800

    added mongodb, ability to charge sword, etc.

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     colors = require('colors'),
+    MongoClient = require('mongodb').MongoClient,
     cors = require('cors');
 
 server.listen(process.env.PORT || 3005);
 
 app.use(cors());
+
+
 
 var link_direction = 'up';
 var players = {};
@@ -16,37 +19,56 @@ var players = {};
 io.on('connection', (socket) => {
     socket.on('game-start', (user) => {
         console.log('user has connected.');
-        player = {
-            player: user,
-            position: {
-                //x: Math.floor(Math.random() * (1920/4)+1),
-                //y: Math.floor(Math.random() * (1080/4)+1)
-                x: 100,
-                y: 100
-            },
-            status: 5,
-            direction: 'up',
-            health: 3,
-            max_health: 3,
-            socket_id: socket.id
-            
-        }
-        players[user] = player;
 
-        //emit the player status to all the clients so that they add it to their array
-        io.emit('add-player', players);
+        //get user from mongodb if they exist
+        find_player(user, (player) => {
+            if (player) {
+                players[user] = player;
+                players[user].socket_id = socket.id;
+            }
+            else {
+                //use the default player settings, this is a new player
+                var player = {
+                    player: user,
+                    position: {
+                        x: 11,
+                        y: 11
+                    },
+                    status: 5,
+                    direction: 'up',
+                    health: 3,
+                    max_health: 3,
+                    socket_id: socket.id   
+                }
+
+                players[user] = player;
+            }
+            
+            //emit the player status to all the clients so that they add it to their array
+            io.emit('add-player', players);
+        })
+
+        
+        
     })
     socket.on('disconnect', () => {
         for (var prop in players) {
             if (players.hasOwnProperty(prop)) {
                 if (players[prop].socket_id == socket.id) {
-                    delete players[prop];
-
-                    io.emit('disconnect', prop);
+                    
+                    savePlayerInfo(prop, () => { 
+                        delete players[prop];
+                        io.emit('disconnect', prop);
+                    });
                 }
             }
         }
-    })
+    });
+
+    socket.on('charge-sword', (player) => {
+        socket.broadcast.emit('charge-sword', player);
+    });
+
     socket.on('player-message', (message) => {
 
     });
@@ -61,16 +83,20 @@ io.on('connection', (socket) => {
 
     socket.on('move-link', (data) => {
 
+        console.log('received move-link event, data = ');
+        console.log(data);
+
+        if (data.charging_sword != undefined) {
+            players[data.player].charging_sword = data.charging_sword;
+        }
+
         if (players[data.player]) {
             if (data.direction == players[data.player].direction) {
                 players[data.player].status++;
                 players[data.player].status = players[data.player].status % 6;
 
-                console.log('player status = ' + players[data.player].status);
-
-                //detect a collision and get new position
-                [players[data.player].position, players[data.player].collision] = detect_player_collision(data.player, players[data.player].position, data.direction);
-
+                //detect a collision and get new position for player
+                detect_player_collision(data.player, data.direction);
             }
             else {
                 delete players[data.player].collision
@@ -78,12 +104,16 @@ io.on('connection', (socket) => {
                 console.log('changing orientation only');
                 players[data.player].status = 3;
                 players[data.player].direction = data.direction;
-
-                console.log(players[data.player]);
             }
-            console.log('__________________ EMITTING _________'.bold)
-            console.log(players[data.player].position.x + ', ' + players[data.player].position.y);
-            io.emit('position', players[data.player]);
+
+
+            if (players[data.player]) {
+                //if the player is still alive and no collisions occured then emit new position
+
+                //change the direction of link to one of the four directions if it's a diagonal. we can just check if the prefix is "north" or "south"
+
+                io.emit('position', players[data.player]);
+            }
         }
     })
 })
@@ -102,12 +132,22 @@ function detect_sword_collision(player) {
     for (var prop in players) {
         if (players.hasOwnProperty(prop) && prop != player) {
             var other_position = players[prop].position;
-            if (is_collision(sword_position, other_position, 6)) {
+            if (is_collision(sword_position, other_position, 25)) {
                 players[prop].health -= 0.5;
 
                 if (players[prop].health <= 0.0) {
-                    io.emit('player_death', prop);
-                    delete players[prop];
+                    
+                    console.log('prop = ' + prop + ", should be a-cool-girl");
+                    
+
+                    reset_player_stats(prop);
+
+                    var username = prop;
+
+                    savePlayerInfo(username, () => {
+                        delete players[username];
+                        io.emit('player_death', username);
+                    });
                 }
                 else {
                     players[prop].position = get_position(players[prop].position, players[player].direction, false, true);
@@ -126,18 +166,107 @@ function detect_sword_collision(player) {
 
 }
 
-function is_collision(pos1, pos2, tolerance = 5) {
+function is_collision(pos1, pos2, tolerance = 15) {
     return Math.abs(pos1.x-pos2.x) < tolerance && Math.abs(pos1.y-pos2.y) < tolerance;
 }
 
-function detect_player_collision(player, position, direction) {
+function savePlayerInfo(player, callback) {
+    console.log("saving player info for " + player);
+    //first check to make sure that the user exists in DB. if it does, then do an update, otherwise, insert
+    if (players[player]) {
+        if (players[player].socket_id) {
+            delete players[player].socket_id;
+        }
+        if (players[player].collision) {
+            delete players[player].collision
+        }
+        connect_db((db) => {
+            db.collection('players').find({
+                'player': player
+            })
+            .toArray((err, players) => {
+                console.log(players);
+                if (players.length) {
+                    //do an update
+                    console.log("updating player....");
+                    updatePlayer(db, player, (results) => { 
+                        //console.log(results);
+                        callback();
+                    });
+                }
+                else {
+                    console.log("inserting player....");
+                    insertPlayer(db, player, (results) => {
+                        //console.log(results);
+                        callback();
+                    });
+                }
+            })
+        })
+    }
+}
+
+function updatePlayer(db, player, callback) {
+    db.collection('players').update({
+        player: player
+    }, { 
+        $set: players[player]
+    }, (err, results) => {
+        if (err) {
+            console.log(err);
+
+            callback(err);
+        }
+        else {
+            callback(results)
+        }
+    });
+}
+
+function insertPlayer(db, player, callback) {
+    db.collection('players').insert(players[player], (err, results) => {
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(results);
+        }
+    })
+}
+
+
+function connect_db(callback) {
+    MongoClient.connect(process.env.NODE_DATABASE_URL, function(err, db) {
+        if (!err) {
+            console.log('connected to database successfully')
+            callback(db);
+        } 
+        else  {
+            console.log(err)
+        }
+    })
+
+}
+
+function find_player(player, callback) {
+    connect_db((db) => {
+        db.collection('players').find({ 'player': player})
+        .toArray((err, players) => {
+            callback(players[0]);
+        })
+    })
+}
+
+function detect_player_collision(player, direction) {
     var your_position;
 
+    var collision = false;
 
-    your_position = get_position(position, direction);
+
+    your_position = get_position(players[player].position, direction);
 
 
-    console.log((position.x + ', ' + position.y).red);
+    console.log((players[player].position.x + ', ' + players[player].position.y).red);
     console.log((your_position.x + ', ' + your_position.y).green);
 
     for (var prop in players) {
@@ -146,27 +275,68 @@ function detect_player_collision(player, position, direction) {
             var other_position = players[prop].position;
 
             if (is_collision(your_position, other_position)) {
-                console.log('collision detected, pushing link back');
-                your_position = get_position(position, direction, true);
-
+                collision = true;
                 players[player].health -= 0.5;
                 players[prop].health -= 0.5;
 
-                players[prop].position = get_position(other_position, direction, false, true);
+                if (players[prop].health <= 0.0) {
+                    
+                    
+                    //reset position and health to defaults since person died
+                    reset_player_stats(prop);
 
-                io.emit('collision', { 
-                    username: prop,
-                    player: players[prop]
-                });
+                    console.log('prop = ' + prop + "(should be groovy-comrade)"); 
 
-                return [your_position, true];
+                    savePlayerInfo(prop, () => { 
+                        delete players[prop]
+                        io.emit('player_death', prop);
+                    });
+                }
+                else {
+                    players[prop].position = get_position(other_position, direction, false, true);
+
+                    io.emit('collision', { 
+                        username: prop,
+                        player: players[prop]
+                    });
+                }
+
+                if (players[player].health <= 0.0) {
+                    console.log('player died, emitting death event');
+                    
+                    
+
+                    reset_player_stats(player);
+
+                    savePlayerInfo(player, () => { 
+                        delete players[player];
+                        io.emit('player_death', player);
+                    });
+                }
+                else {
+                    console.log('collision detected, pushing link back');
+                    players[player].position = get_position(players[player].position, direction, true);
+                    players[player].collision = true;
+                }
+                break;
             }
         }
     }
 
     console.log('returning new position');
+    if (!collision) {
+        delete players[player].collision;
+        players[player].position = your_position;
+    }
+    
+}
 
-    return [your_position, false];
+function reset_player_stats(player) {
+    //reset position and health to defaults since person died
+    players[player].health = players[player].max_health;
+    players[player].position.x = 100;
+    players[player].position.y = 100;
+    delete players[player].collision;
 }
 
 function get_position(position, direction, detect_collision = false, is_opponent_collision = false) {
@@ -174,20 +344,20 @@ function get_position(position, direction, detect_collision = false, is_opponent
 
     //used for detecting sword collisions, since it uses the same code as getting the normal position, except displace a little more
     //hope this makes sense to future self
-    var displacement = is_opponent_collision ? 3 : 1;
+    var displacement = is_opponent_collision ? 6 : 1;
 
     switch(direction) {
         case 'up':
             return_position = {
                 x: position.x,
-                y: detect_collision ? position.y+3 : position.y-displacement,
+                y: detect_collision ? position.y+6 : position.y-displacement,
             };
         break;
         case 'down':
 
             return_position = {
                 x: position.x,
-                y: detect_collision ? position.y-3 : position.y+displacement,
+                y: detect_collision ? position.y-6 : position.y+displacement,
             };
             
 
@@ -195,7 +365,7 @@ function get_position(position, direction, detect_collision = false, is_opponent
         case 'left':
 
             return_position = {
-                x: detect_collision ? position.x+3 : position.x-displacement,
+                x: detect_collision ? position.x+6 : position.x-displacement,
                 y: position.y
             };   
             
@@ -204,11 +374,36 @@ function get_position(position, direction, detect_collision = false, is_opponent
         case 'right':
 
             return_position = {
-                x: detect_collision ? position.x-3 : position.x+displacement,
+                x: detect_collision ? position.x-6 : position.x+displacement,
                 y: position.y
-            }; 
-            
-            
+            };   
+        break;
+        case 'northwest':
+            console.log('northwest detected');
+            return_position = {
+                x: detect_collision ? position.x+6 : position.x-displacement,
+                y: detect_collision ? position.y+6 : position.y-displacement
+            }
+
+        break;
+        case 'northeast':
+            return_position = {
+                x: detect_collision ? position.x-6 : position.x+displacement,
+                y: detect_collision ? position.y+6 : position.y-displacement
+            }
+
+        break;
+        case 'southwest':
+            return_position = {
+                x: detect_collision ? position.x+6 : position.x-displacement,
+                y: detect_collision ? position.y-6 : position.y+displacement
+            }
+        break;
+        case 'southeast':
+            return_position = {
+                x: detect_collision ? position.x-6 : position.x+displacement,
+                y: detect_collision ? position.y-6 : position.y+displacement
+            }
         break;
     }
 
